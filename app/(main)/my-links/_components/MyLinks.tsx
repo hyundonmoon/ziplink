181f4b153b9b5e552382cf1c6f34334e1b9f3640
Hyun Don Moon <hyundonmoon@gmail.com>
2024-10-15 16:36:06 +0900

    change my links page style and send to sign in page when user clicks on sign in button

--- app/(main)/my-links/_components/MyLinks.tsx
+++ app/(main)/my-links/_components/MyLinks.tsx
@@ -1,5 +1,6 @@
 import ShortUrlLink from '@/app/(main)/my-links/_components/ShortUrlLink';
 import { getUserLinks as getUserLinksServerAction } from '@/app/lib/actions';
+import Link from 'next/link';
 
 export default async function MyLinks() {
 	const links = await getUserLinksServerAction();
@@ -8,19 +9,25 @@ export default async function MyLinks() {
 		return (
 			<p className="text-center py-16">
 				You haven&#39;t shortened any links.{' '}
-				<a href="/" className="hover:border-b border-current">
+				<Link href="/" className="hover:border-b border-current">
 					Create one
-				</a>
+				</Link>
 				?
 			</p>
 		);
 	}
 
 	return (
-		<ul className="space-y-4">
-			{links.map((link) => (
-				<ShortUrlLink key={link.id.toString()} link={link} />
-			))}
-		</ul>
+		<>
+			<h1 className="text-2xl font-bold mb-4 pb-4 border-b">
+				Your links
+			</h1>
+
+			<ul className="space-y-4">
+				{links.map((link) => (
+					<ShortUrlLink key={link.id.toString()} link={link} />
+				))}
+			</ul>
+		</>
 	);
 }
--- app/(main)/my-links/page.tsx
+++ app/(main)/my-links/page.tsx
@@ -1,24 +1,32 @@
 import MyLinks from '@/app/(main)/my-links/_components/MyLinks';
 import MyLinksSkeleton from '@/app/(main)/my-links/_components/MyLinksSkeleton';
 import { auth } from '@/auth';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function MyLinksPage() {
 	const session = await auth();
+	const signInLink = `/auth/signin?callbackUrl=${encodeURIComponent(
+		'/my-links'
+	)}`;
 
 	return (
 		<div>
-			<h1 className="text-2xl font-bold mb-4 pb-4 border-b">
-				Your links
-			</h1>
-
 			{session ? (
 				<Suspense fallback={<MyLinksSkeleton />}>
 					<MyLinks />
 				</Suspense>
 			) : (
 				<div className="text-center py-16">
-					<p>Sign in to view your links</p>
+					<p>
+						<Link
+							href={signInLink}
+							className="hover:border-b border-current"
+						>
+							Sign in
+						</Link>{' '}
+						to view your links
+					</p>
 				</div>
 			)}
 		</div>
